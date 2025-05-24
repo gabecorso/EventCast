105dd47b5c90593eea36630531e916cc96b46b22
Gabe Corso <gabecorso@gmail.com>
2025-05-24 15:55:03 -0400

    set up weather data and initial fetch request upon app load

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ErrorBoundary from './components/ErrorBoundary';
 import LoadingState from './components/LoadingState';
+import config from './config/config';
 import { WeatherProvider } from './context/WeatherContext';
-import { WeatherContextType } from './types';
+import { WeatherContextType, WeatherData, TimeRange } from './types';
 
 function App() {
+
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  const [location, setLocation] = useState<string>(config.DEFAULT_LOCATION);
+  const [selectedDay, setSelectedDay] = useState<number>(config.DEFAULT_DAY);
+  const [selectedTimeRange, setSelectedTimeRange] = useState<TimeRange>(config.DEFAULT_TIME_RANGES[1]); // Afternoon by default
+
 
   const contextValue: WeatherContextType = {
     weatherData,
